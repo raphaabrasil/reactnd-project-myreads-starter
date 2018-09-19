3ff88ca6f360c738bde15341b92af0871279338a
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-09-19 17:59:26 -0300

    Changes Book component to use props to mount content

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import AddIcon from '@material-ui/icons/Add'
+import PropTypes from 'prop-types'
+
 
 class Book extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+    authors: PropTypes.array,
+    imageLinks: PropTypes.object,
+  }
+
   render() {
+    const { title, subtitle, authors, imageLinks } = this.props
     return (
-      <Card style={{ display: 'flex' }}>
+      <Card style={{ display: 'flex', height: '100%' }}>
         <CardMedia
           component="img"
           className="book-cover"
-          image="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+          image={ imageLinks.thumbnail }
           title="Contemplative Reptile"
         />
-        <div>
+        <div className="book-description">
           <CardContent>
-            <Typography gutterBottom  variant="headline" component="h2">
-              Lizard
+            <Typography variant="headline" component="h2">
+              { title }
             </Typography>
-            <Typography component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
+            { subtitle && (
+              <Typography variant="subheading" component="h3">
+                { subtitle }
+              </Typography>
+            )}
+            <Typography paragraph component="p" className="book-authors">
+              { authors.map( (author, idx) => (
+                idx === 0 ? author :  `, ${author}`
+              ))}
             </Typography>
           </CardContent>
           <CardActions style={{ justifyContent: 'flex-end' }}>
