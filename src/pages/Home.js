f24e315309e75658b15d1be0e9dea912e52aea76
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-09-18 23:44:47 -0300

    Adds link for search page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from  'react-router-dom'
 import Book from '../components/book/Book'
 
 class Home extends Component {
@@ -55,7 +56,7 @@ class Home extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
