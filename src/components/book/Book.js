8d66767617d5d9376c6c3a15f281f8cb095e2201
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-09-19 16:36:07 -0300

    Created card layout for book list

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
 
 class Book extends Component {
   render() {
     return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+      <Card style={{ display: 'flex' }}>
+        <CardMedia
+          component="img"
+          className="book-cover"
+          image="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+          title="Contemplative Reptile"
+        />
+        <div>
+          <CardContent>
+            <Typography gutterBottom  variant="headline" component="h2">
+              Lizard
+            </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions style={{ justifyContent: 'flex-end' }}>
+            <Button color="primary" style={{ fontSize: 12 }} aria-label="Delete">
+              <AddIcon style={{ fontSize: 18, marginRight: 4 }}/>
+              To Read
+            </Button>
+            <Button color="primary" style={{ fontSize: 12 }} aria-label="Delete">
+              <AddIcon style={{ fontSize: 18, marginRight: 4 }}/>
+              Reading
+            </Button>
+            <Button color="primary" style={{ fontSize: 12 }} aria-label="Delete">
+              <AddIcon style={{ fontSize: 18, marginRight: 4 }}/>
+              Read
+            </Button>
+          </CardActions>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
-      </div>
+      </Card>
     )
   }
 }
